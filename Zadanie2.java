ff6afda613d1b4e7118eb6e226e4ebab884629ee
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2021-10-06 12:38:29 +0200

    Add files via upload

---
+++ Zadanie2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Zadanie2 {
+    public static void main(String[] args) {
+        int suma = 0;
+        for (int i = 0; i <= 10; i++) {
+            suma += i;
+        }
+        System.out.printf("%d", suma);
+    }
+}
