153ef23cf8e1ee600b7e077ad0b86934b34d6db3
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2021-10-09 13:33:29 +0200

    Add files via upload

---
+++ zad3.java
@@ -0,0 +1,9 @@
+public class zad3 {
+    public static void main(String[] args) {
+        int iloczyn = 1;
+        for (int i = 1; i <= 10; i++) {
+            iloczyn *= i;
+        }
+        System.out.println(iloczyn);
+    }
+}
---
+++ zad5.java
@@ -0,0 +1,5 @@
+public class zad5 {
+    public static void main(String[] args) {
+        System.out.print(" _____\n|JAVA|\n -----");
+    }
+}
