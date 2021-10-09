fd100a0055394781935e7d06132e120f5818cd0b
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2021-10-09 13:30:45 +0200

    Delete zad3.java

--- zad3.java
+++
@@ -1,9 +0,0 @@
-public class zad3 {
-    public static void main(String[] args) {
-        int iloczyn = 1;
-        for (int i = 1; i <= 10; i++) {
-            iloczyn *= i;
-        }
-        System.out.println(iloczyn);
-    }
-}
