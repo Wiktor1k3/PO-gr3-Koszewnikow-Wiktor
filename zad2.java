572e87055d01421b128a7bd21a73f875aecb982b
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2021-10-12 16:55:58 +0200

    Delete zad2.java

--- zad2.java
+++
@@ -1,9 +0,0 @@
-public class zad2 {
-    public static void main(String[] args) {
-        int suma = 0;
-        for (int i = 1; i <= 10; i++) {
-            suma += i;
-        }
-        System.out.println(suma);
-    }
-}
