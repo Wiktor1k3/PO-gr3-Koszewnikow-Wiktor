b63f8a59689dccef2dbdb89d641a62077ff5450e
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2021-11-30 19:41:30 +0100

    Create BetterRectangle.java

---
+++ lab7/BetterRectangle.java
@@ -0,0 +1,19 @@
+package pl.imiaj.koszewnikow;
+
+import java.awt.*;
+
+public class BetterRectangle extends Rectangle {
+
+    public BetterRectangle(int x, int y, int height, int width){
+        super(x, y, width, height);
+    }
+
+    public int getPerimeter(){
+        return 2*width+2*height;
+
+    }
+
+    public int getArea(){
+        return width*height;
+    }
+}
