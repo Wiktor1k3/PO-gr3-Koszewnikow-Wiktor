2ffd16f861b0fedcc614b3fd461d13c73a9d7ab0
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2021-12-30 20:55:45 +0100

    Create PairUtil.java

---
+++ lab11/pl.imiaj.koszewnikow/Pair/PairUtil.java
@@ -0,0 +1,15 @@
+package pl.imiaj.koszewnikow.Pair;
+
+public class PairUtil<T>{
+
+
+
+
+    public static <T> Pair swap(Pair p){
+        Pair pair = new Pair(p.getSecond(), p.getFirst());
+
+
+        return pair;
+    }
+
+}
