c0cfdd467db0c1f113b63e89b4cf0c9576fca334
Wiktor Koszewników <73649635+Wiktor1k3@users.noreply.github.com>
2021-12-30 20:54:33 +0100

    Create ArrayUtil.java

---
+++ lab11/pl.imiaj.koszewnikow/ArrayUtil/ArrayUtil.java
@@ -0,0 +1,19 @@
+package pl.imiaj.koszewnikow.ArrayUtil;
+
+import java.util.ArrayList;
+
+public class ArrayUtil<T> {
+
+
+
+    public static <T extends Comparable<T>> void isSorted(ArrayList<T> ar){
+
+        for(int i = 0; i <= ar.size()-2; i++){
+            if(ar.get(i).compareTo(ar.get(i+1)) > 0) {
+                System.out.println("Posortowana niemalejaco");
+                return;
+            }
+        }
+        System.out.println("Posortowana niemalejaco");
+    }
+}
